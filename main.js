2ed3ad7067729219a46a75cfcbc627628d8564ca
Shakeel Haider <shkhaider2015@gmail.com>
2024-05-02 18:47:33 +0500

    add image to the last

--- main.js
+++ main.js
@@ -1,42 +1,46 @@
-import './style.css';
-import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-import { RoundedBoxGeometry } from 'three/addons/geometries/RoundedBoxGeometry.js'
+import "./style.css";
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { RoundedBoxGeometry } from "three/addons/geometries/RoundedBoxGeometry.js";
 
 // Setup
 
-const progressBar = document.getElementById('progress-bar');
-const progressBarContainer = document.getElementsByClassName('progress-bar-container')
-
+const progressBar = document.getElementById("progress-bar");
+const progressBarContainer = document.getElementsByClassName(
+  "progress-bar-container"
+);
 
 const loadingManager = new THREE.LoadingManager();
 loadingManager.onStart = function (url, loaded, total) {
-  console.log("Start : ", url, loaded, total)
-}
+  console.log("Start : ", url, loaded, total);
+};
 loadingManager.onProgress = function (url, loaded, total) {
-  console.log("Progress : ", url, loaded, total)
-  progressBar.value = (loaded/total) * 100;
-}
+  console.log("Progress : ", url, loaded, total);
+  progressBar.value = (loaded / total) * 100;
+};
 loadingManager.onLoad = function () {
-  console.log("Loaded : ", progressBarContainer)
-  if(progressBarContainer.length > 0) {
-    progressBarContainer[0].style.display = 'none';
+  console.log("Loaded : ", progressBarContainer);
+  if (progressBarContainer.length > 0) {
+    progressBarContainer[0].style.display = "none";
   }
-  
-}
+};
 loadingManager.onError = function (url) {
-  console.log("Error : ", url)
-  if(progressBarContainer.length > 0) {
-    progressBarContainer[0].style.display = 'none';
+  console.log("Error : ", url);
+  if (progressBarContainer.length > 0) {
+    progressBarContainer[0].style.display = "none";
   }
-}
+};
 const scene = new THREE.Scene();
 
-
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
 
 const renderer = new THREE.WebGLRenderer({
-  canvas: document.querySelector('#bg'),
+  canvas: document.querySelector("#bg"),
 });
 
 renderer.setPixelRatio(window.devicePixelRatio);
@@ -56,7 +60,7 @@ scene.add(torus);
 
 // Lights
 
-const pointLight = new THREE.PointLight(0xffffff, 100,20, 2);
+const pointLight = new THREE.PointLight(0xffffff, 100, 20, 2);
 pointLight.position.set(2, 1, 3);
 
 const ambientLight = new THREE.AmbientLight(0xffffff, 2);
@@ -72,11 +76,13 @@ scene.add(pointLight, ambientLight);
 // scene.add(lightHelper, gridHelper)
 
 // const controls = new OrbitControls(camera, renderer.domElement);
-const normalTexture = new THREE.TextureLoader(loadingManager).load('normal.jpg');
+const normalTexture = new THREE.TextureLoader(loadingManager).load(
+  "normal.jpg"
+);
 
 function addStar() {
   const geometry = new THREE.SphereGeometry(0.25, 24, 24);
-  const material = new THREE.MeshStandardMaterial({ color: 0xffffff});
+  const material = new THREE.MeshStandardMaterial({ color: 0xffffff });
   const star = new THREE.Mesh(geometry, material);
 
   const [x, y, z] = Array(3)
@@ -91,23 +97,30 @@ Array(200).fill().forEach(addStar);
 
 // Background
 
-const spaceTexture = new THREE.TextureLoader(loadingManager).load('space.jpg');
+const spaceTexture = new THREE.TextureLoader(loadingManager).load("space.jpg");
 scene.background = spaceTexture;
 
 // Avatar
 
-// const shakeelHaiderTexture = new THREE.TextureLoader(loadingManager).load('shakeel_haider.jpg');
+const shakeelHaiderTexture = new THREE.TextureLoader(loadingManager).load(
+  "shakeel_haider.jpg"
+);
 
-// const shakeelHaider = new THREE.Mesh(new RoundedBoxGeometry(3, 3, 3), new THREE.MeshBasicMaterial({ map: shakeelHaiderTexture }));
+const shakeelHaider = new THREE.Mesh(
+  new RoundedBoxGeometry(3, 3, 3),
+  new THREE.MeshBasicMaterial({ map: shakeelHaiderTexture })
+);
 
-// scene.add(shakeelHaider);
+scene.add(shakeelHaider);
 
 // earth
-const earthTexture = new THREE.TextureLoader(loadingManager).load('flat_earth03.jpeg');
+const earthTexture = new THREE.TextureLoader(loadingManager).load(
+  "flat_earth03.jpeg"
+);
 
 const earthGeo = new THREE.SphereGeometry(2, 40, 40);
 const earthMesh = new THREE.MeshStandardMaterial({
-  map: earthTexture
+  map: earthTexture,
 });
 
 const earth = new THREE.Mesh(earthGeo, earthMesh);
@@ -115,7 +128,7 @@ scene.add(earth);
 
 // Moon
 
-const moonTexture = new THREE.TextureLoader(loadingManager).load('moon.jpg');
+const moonTexture = new THREE.TextureLoader(loadingManager).load("moon.jpg");
 
 const moon = new THREE.Mesh(
   new THREE.SphereGeometry(3, 32, 32),
@@ -127,7 +140,9 @@ const moon = new THREE.Mesh(
 
 scene.add(moon);
 
-const marsTexture = new THREE.TextureLoader(loadingManager).load('Mars_Map.webp');
+const marsTexture = new THREE.TextureLoader(loadingManager).load(
+  "Mars_Map.webp"
+);
 
 const mars = new THREE.Mesh(
   new THREE.SphereGeometry(3, 44, 44),
@@ -135,9 +150,9 @@ const mars = new THREE.Mesh(
     map: marsTexture,
     normalMap: normalTexture,
   })
-); 
+);
 
-scene.add(mars)
+scene.add(mars);
 
 moon.position.z = 30;
 moon.position.setX(-10);
@@ -145,8 +160,10 @@ moon.position.setX(-10);
 mars.position.z = 50;
 mars.position.setX(-20);
 
-// shakeelHaider.position.z = -5;
-// shakeelHaider.position.x = 2;
+shakeelHaider.position.z = 60;
+shakeelHaider.position.x = -20;
+shakeelHaider.position.setY(-5)
+shakeelHaider.rotateY(20)
 
 earth.position.z = -5;
 earth.position.x = 2;
@@ -166,7 +183,7 @@ function moveCamera() {
   // shakeelHaider.rotation.y += 0.01;
   // shakeelHaider.rotation.z += 0.01;
 
-   earth.rotation.y += 0.01;
+  earth.rotation.y += 0.01;
   earth.rotation.z += 0.01;
 
   camera.position.z = t * -0.01;
